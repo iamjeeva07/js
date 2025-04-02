6387e1459428c40c4fb765eec6c4c87b1fa249e8
iamjeeva07 <thejeeva07@gmail.com>
2025-04-02 13:00:33 +0530

    created files

---
+++ introduction/script.js
@@ -0,0 +1,9 @@
+// let amount=1000;// variavle initialization
+// const pi=3.14; // contatnt initialization
+// var value=10; // varible initialization old method
+// console.log("your bill amount is Rs."+ amount); // to display a mesage in consoole used for testing purpose 
+// alert("hii"); // to show alert
+
+
+// let j=prompt("how old are you?");
+// console.log("you are eligible",Number(j)+5); 
